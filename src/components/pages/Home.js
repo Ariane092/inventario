b0a04ac95d068bf07472c320fc35a69eb3ec232f
Ariane092 <arianediniz922@gmail.com>
2024-01-26 18:01:54 -0300

    alterações home page table

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -68,7 +68,7 @@ const columns = [
 
 function Home() {
   const [dados, setDados] = useState([]);
-  const [filtering, setFiltering] = useState("");
+  const [globalFilter, setGlobalFilter] = useState("");
   const [columnFilters, setColumnFilters] = useState([]);
   const { Search } = Input;
 
@@ -76,16 +76,18 @@ function Home() {
     columns,
     data: dados,
     state: {
-      globalFilter: filtering,
-      columnFilters,
+      globalFilter: globalFilter,
+      columnFilters: columnFilters,
     },
-    onGlobalFilterChanged: setFiltering,
+    onGlobalFilterChanged: setGlobalFilter,
     onColumnFilterChanged: setColumnFilters,
     getFilteredRowModel: getFilteredRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
     getSortedRowModel: getSortedRowModel(),
     getCoreRowModel: getCoreRowModel(),
   });
+
+  
 
   useEffect(() => {
     const fetchData = async () => {
@@ -106,14 +108,14 @@ function Home() {
     <div className="table-container">
       <h4>Lista de Equipamentos</h4>
 
-        <Search
-          placeholder="Pesquisar"
-          allowClear
-          value={filtering}
-          onChange={(e) => setFiltering(e.target.value)}
-          className="global-search"
-          style={{ width: 200, marginTop: 40 }}
-        />
+      <Search
+        placeholder="Pesquisar"
+        allowClear
+        value={globalFilter}
+        onChange={(e) => setGlobalFilter(e.target.value)}
+        className="global-search"
+        style={{ width: 200, marginTop: 40 }}
+      />
 
       <div className="bloc-tabs">
         <div className="tabs">
@@ -142,10 +144,9 @@ function Home() {
                     onClick={header.column.getToggleSortingHandler()}
                   />
                 )}
-                    <Search
-                      allowClear    
-                      style={{ width: 100 }}
-                    />
+                {header.column.getCanFilter() ? (
+                  
+                ) : null}
               </th>
             ))}
           </thead>
@@ -182,6 +183,109 @@ function Home() {
       </div>
     </div>
   );
+}
+
+function DebouncedInput({
+  value: initialValue,
+  onChange,
+  debounce = 500,
+  ...props
+}) {
+  const [value, setValue] = React.useState(initialValue);
+
+  React.useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
+
+  React.useEffect(() => {
+    const timeout = setTimeout(() => {
+      onChange(value);
+    }, debounce);
+
+    return () => clearTimeout(timeout);
+  }, [value]);
+
+  return(
+    <Search
+    allowClear
+    style={{ width: 115 }}
+    {...props}
+    value={value}
+    onChange={(e) => setValue(e.target.value)}
+    />
+  );
+}
+
+function Filter({ column, table }) {
+ 
+  const firstValue = table
+    .getPreFilteredRowModel()
+    .flatRows[0]?.getValue(column.id);
+
+  const columnFilterValue = column.getFilterValue();
+
+  const sortedUniqueValues = React.useMemo(
+    () =>
+      typeof firstValue === 'number'
+        ? []
+        : Array.from(column.getFacetedUniqueValues().keys()).sort(),
+    [column.getFacetedUniqueValues()]
+  )
+
+  return typeof firstValue === 'number' ? (
+    <div>
+      <div className="flex space-x-2">
+        <DebouncedInput
+          type="number"
+          min={Number(column.getFacetedMinMaxValues()?.[0] ?? '')}
+          max={Number(column.getFacetedMinMaxValues()?.[1] ?? '')}
+          value={(columnFilterValue as [number, number])?.[0] ?? ''}
+          onChange={value =>
+            column.setFilterValue((old: [number, number]) => [value, old?.[1]])
+          }
+          placeholder={`Min ${
+            column.getFacetedMinMaxValues()?.[0]
+              ? `(${column.getFacetedMinMaxValues()?.[0]})`
+              : ''
+          }`}
+          className="w-24 border shadow rounded"
+        />
+        <DebouncedInput
+          type="number"
+          min={Number(column.getFacetedMinMaxValues()?.[0] ?? '')}
+          max={Number(column.getFacetedMinMaxValues()?.[1] ?? '')}
+          value={(columnFilterValue as [number, number])?.[1] ?? ''}
+          onChange={value =>
+            column.setFilterValue((old: [number, number]) => [old?.[0], value])
+          }
+          placeholder={`Max ${
+            column.getFacetedMinMaxValues()?.[1]
+              ? `(${column.getFacetedMinMaxValues()?.[1]})`
+              : ''
+          }`}
+          className="w-24 border shadow rounded"
+        />
+      </div>
+      <div className="h-1" />
+    </div>
+  ) : (
+    <>
+      <datalist id={column.id + 'list'}>
+        {sortedUniqueValues.slice(0, 5000).map((value: any) => (
+          <option value={value} key={value} />
+        ))}
+      </datalist>
+      <DebouncedInput
+        type="text"
+        value={(columnFilterValue ?? '')}
+        onChange={value => column.setFilterValue(value)}
+        placeholder={`Search... (${column.getFacetedUniqueValues().size})`}
+        className="w-36 border shadow rounded"
+        list={column.id + 'list'}
+      />
+      <div className="h-1" />
+    </>
+  )
 }
 
 export default Home;
